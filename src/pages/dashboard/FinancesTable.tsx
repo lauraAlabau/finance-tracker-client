63b0a470f2696e1f66e6031f85f8c9211fc04f50
lauraAlabau <arlaura3@gmail.com>
2024-06-11 18:09:40 +0200

    add table component

---
+++ src/pages/dashboard/FinancesTable.tsx
@@ -0,0 +1,171 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useMemo, useState } from "react";
+import {
+  FinanceRecord,
+  useFinanceContext,
+} from "../../contexts/financeContext";
+import { useTable, Column, CellProps } from "react-table";
+
+interface EditableCellProps extends CellProps<FinanceRecord> {
+  updateRecord: (rowIndex: number, columnId: string, value: any) => void;
+  editable: boolean;
+}
+
+const EditableCell: React.FC<EditableCellProps> = ({
+  value: initialValue,
+  row,
+  column,
+  updateRecord,
+  editable,
+}) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [value, setValue] = useState(initialValue);
+
+  const onBlur = () => {
+    setIsEditing(false);
+    updateRecord(row.index, column.id, value);
+  };
+  return (
+    <div
+      onClick={() => editable && setIsEditing(true)}
+      className={`${editable ? "cursor-pointer" : ""}`}
+    >
+      {isEditing ? (
+        <input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          autoFocus
+          onBlur={onBlur}
+          className="w-full p-2.5 bg-transparent border border-teal-100 rounded-md text-slate-50"
+        />
+      ) : (
+        <div className="p-2.5 w-full">{String(value)}</div>
+      )}
+    </div>
+  );
+};
+export const FinancesTable = () => {
+  const { records, updateRecord, deleteRecord } = useFinanceContext();
+
+  const updateCellRecord = (rowIndex: number, columnId: string, value: any) => {
+    const id = records[rowIndex]?._id;
+    updateRecord(id ?? "", { ...records[rowIndex], [columnId]: value });
+  };
+
+  const columns: Array<Column<FinanceRecord>> = useMemo(
+    () => [
+      {
+        Header: "Description",
+        accessor: "description",
+        Cell: (props) => (
+          <EditableCell
+            {...props}
+            updateRecord={updateCellRecord}
+            editable={true}
+          />
+        ),
+      },
+      {
+        Header: "Amount",
+        accessor: "amount",
+        Cell: (props) => (
+          <EditableCell
+            {...props}
+            updateRecord={updateCellRecord}
+            editable={true}
+          />
+        ),
+      },
+      {
+        Header: "Category",
+        accessor: "category",
+        Cell: (props) => (
+          <EditableCell
+            {...props}
+            updateRecord={updateCellRecord}
+            editable={true}
+          />
+        ),
+      },
+      {
+        Header: "Payment",
+        accessor: "payment",
+        Cell: (props) => (
+          <EditableCell
+            {...props}
+            updateRecord={updateCellRecord}
+            editable={true}
+          />
+        ),
+      },
+      {
+        Header: "Date",
+        accessor: "date",
+        Cell: (props) => (
+          <EditableCell
+            {...props}
+            updateRecord={updateCellRecord}
+            editable={false}
+          />
+        ),
+      },
+      {
+        Header: "Delete",
+        id: "delete",
+        Cell: ({ row }) => (
+          <button onClick={() => deleteRecord(row.original._id ?? "")}>
+            Delete
+          </button>
+        ),
+      },
+    ],
+    [records]
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      columns,
+      data: records,
+    });
+
+  return (
+    <div className="my-5 overflow-x-auto">
+      <table
+        {...getTableProps()}
+        className="w-full border-collapse mt-5 shadow-md"
+      >
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th
+                  {...column.getHeaderProps()}
+                  className="py-3 px-4 first-of-type:rounded-tl-md last-of-type:rounded-tr-md    text-left bg-teal-800 text-teal-50 text-base"
+                >
+                  {column.render("Header")}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr
+                {...row.getRowProps()}
+                className=" border-b border-teal-800/40 bg-teal-800/10"
+              >
+                {row.cells.map((cell) => (
+                  <td {...cell.getCellProps()} className="py-3 px-4 text-left">
+                    {cell.render("Cell")}
+                  </td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
