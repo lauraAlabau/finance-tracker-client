0e6bcfb15ec2bea4c8a9208f3b24b6e3c0090736
lauraAlabau <arlaura3@gmail.com>
2024-07-13 21:05:20 +0200

    fix home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,20 @@
 import { SignInButton, SignUpButton } from "@clerk/clerk-react";
+import logo from "../../assets/logos/logo-home.svg";
 
 export const Home = () => {
   return (
-    <div className="flex flex-col items-center md:pt-96">
-      <div className="p-20 -mt-32 text-6xl font-bold text-center lg:text-9xl text-teal-50/90 lg:-mt-60 ">
+    <div className="flex flex-col items-center justify-center min-h-screen lg:-translate-y-10">
+      <div>
+        <img src={logo} className="w-80" alt="BudgetBuddy Logo" />
+      </div>
+      <div className="p-10 text-4xl font-bold text-center lg:pb-0 lg:p-20 lg:text-6xl text-slate-300 lg:mb-20">
         Record your expenses and income easily
       </div>
       <div className="flex gap-8 text-xl font-semibold">
-        <div className="px-5 py-2.5 hover:bg-teal-600 border-2 border-teal-500 rounded-md hover:border-teal-200">
+        <div className="px-5 py-2.5 hover:bg-fuchsia-600 border-2 border-cyan-500 rounded-md hover:border-fuchsia-200">
           <SignInButton mode="modal">Login</SignInButton>
         </div>
-        <div className="px-5 py-2.5 text-teal-900  border-2 bg-teal-500 rounded-md  border-teal-200 hover:border-teal-200 hover:bg-teal-600 hover:text-teal-100">
+        <div className="px-5 py-2.5 text-cyan-900  border-2 bg-cyan-500 rounded-md  border-cyan-200 hover:border-fuchsia-200 hover:bg-fuchsia-600 hover:text-fuchsia-100">
           <SignUpButton mode="modal" />
         </div>
       </div>
