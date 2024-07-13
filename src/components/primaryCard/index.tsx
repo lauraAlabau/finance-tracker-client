73bc5be5514357bb783a9ce574fcb94311ac0648
lauraAlabau <arlaura3@gmail.com>
2024-07-13 14:15:23 +0200

    add neew card UI and styles

--- src/components/primaryCard/index.tsx
+++ src/components/primaryCard/index.tsx
@@ -5,9 +5,20 @@ type TotalMonthProps = {
 
 const PrimaryCard = ({ amount, text }: TotalMonthProps) => {
   return (
-    <div className="border-2 border-teal-100/15 text-lg bg-teal-400/15 rounded-md lg:py-8 lg:px-16 py-2.5 px-5 w-fit shadow-md shadow-black text-white/90 flex flex-col items-center gap-2.5">
-      <div className="lg:text-5xl text-base font-semibold"> {amount} €</div>
-      <div className="lg:text-base text-xs text-center">{text}</div>
+    <div className="flex items-center justify-around rounded-lg bg-[#0B1739] shadow-md shadow-slate-950 lg:py-8 py-4 px-2 border-t-8 border-cyan-400">
+      <div className="flex lg:items-start items-center flex-col gap-2.5 text-lg text-white/90 ">
+        <p className="text-xs text-center lg:text-base text-white/60">{text}</p>
+        <p className="text-base font-semibold lg:text-5xl">
+          {amount} <span className="flex-auto lg:hidden">€</span>
+        </p>
+      </div>
+      <div className="hidden w-12 h-12 py-2 translate-x-3 -translate-y-1 border-2 rounded-full border-fuchsia-500 lg:block">
+        <div className="w-14 h-14 -translate-x-4 -translate-y-1 border-4 border-[#0B1739] rounded-full bg-cyan-500 flex items-center justify-center">
+          <p className="font-semibold text-3xl text-[#0B1739] -translate-x-0.5 ">
+            €
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/totalCard/index.tsx
+++ src/components/totalCard/index.tsx
@@ -5,9 +5,20 @@ type TotalMonthProps = {
 
 const TotalCard = ({ amount, text }: TotalMonthProps) => {
   return (
-    <div className="border-2 border-teal-100/10 text-lg bg-teal-400/10 rounded-md lg:py-6 lg:px-14 py-2 px-4 w-fit shadow-md shadow-black text-white/90 flex flex-col items-center gap-2.5">
-      <div className="text-sm font-semibold lg:text-4xl"> {amount} €</div>
-      <div className="text-xs text-center lg:text-sm">{text}</div>
+    <div className="flex items-center justify-around rounded-lg bg-[#0B1739] shadow-md shadow-slate-950 lg:my-4 lg:mx-4 lg:py-4 py-4 px-2 ">
+      <div className="flex lg:items-start items-center flex-col gap-2.5 text-lg text-white/90 ">
+        <p className="text-xs text-center lg:text-sm text-white/60">{text}</p>
+        <p className="text-sm font-semibold text-center lg:text-4xl">
+          {amount} <span className="flex-auto lg:hidden">€</span>
+        </p>
+      </div>
+      <div className="hidden w-12 h-12 px-4 py-2 translate-x-3 -translate-y-1 border-2 rounded-full border-fuchsia-500 lg:block">
+        <div className="w-14 h-14 -translate-x-8 -translate-y-1.5 border-4 border-[#0B1739] rounded-full bg-cyan-500 flex items-center justify-center">
+          <p className="text-sm font-semibold lg:text-3xl text-[#0B1739] -translate-x-0.5">
+            €
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
